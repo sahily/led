4dc7f215a53993dfb26d17c62dd65ff757817029
Sahil Mahajan <sahilgotsoul@gmail.com>
2019-04-06 01:07:25 -0700

    Fix indents

--- led.c
+++ led.c
@@ -3,10 +3,10 @@
  *
  * Created: 4/5/2019 2:50:40 PM
  * Author : Sahil S. Mahajan
- * Description: Driver for LED
+ * Description: Program to turn on certain LEDs in a row of 8 LEDs
  *
  * Notes: 8 LEDs are connected to Port 0 of the 8051 microcontroller. As an example we can turn on the 3rd, 6th, and 7th LEDs.
- * 			00100110 binary is 26 in hexadecimal, so we can write 0x26 to Port 0.				
+ *          00100110 binary is 26 in hexadecimal, so we can program PORT 0 to be 0x26.              
  *
  */
  
@@ -17,7 +17,7 @@ void main()
 
   while(1)
   {
-		P0=0X26;
+        P0=0X26;
   }
 
 }
