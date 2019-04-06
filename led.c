8e0d99fd28920058b981e343bc3c24aba34ee5ce
Sahil Mahajan <sahilgotsoul@gmail.com>
2019-04-05 18:28:03 -0700

    Add source code and example picture

---
+++ led.c
@@ -0,0 +1,25 @@
+/*
+ * drumReferenceMetronome.c
+ *
+ * Created: 4/5/2019 2:50:40 PM
+ * Author : Sahil S. Mahajan
+ * Description: Program to turn on certain LEDs in a row of 8 LEDs
+ *
+ * Notes: 8 LEDs are connected to Port 0 of the 8051 microcontroller. As an example we can turn on the 3rd, 6th, and 7th LEDs.
+ * 			00100110 binary is 26 in hexadecimal, so we can program PORT 0 to be 0x26.				
+ *
+ */
+ 
+#include <REG51F.H>
+
+void main()
+{
+
+  while(1)
+  {
+		P0=0X26;
+  }
+
+}
+
+
