e77c093421ce1243b4c57eb5329c2cf5dacf4310
sahily <sahilgotsoul@gmail.com>
2019-04-06 01:30:57 -0700

    Edit description

--- led.c
+++ led.c
@@ -3,7 +3,7 @@
  *
  * Created: 4/5/2019 2:50:40 PM
  * Author : Sahil S. Mahajan
- * Description: Program to turn on certain LEDs in a row of 8 LEDs
+ * Description: Driver for LED
  *
  * Notes: 8 LEDs are connected to Port 0 of the 8051 microcontroller. As an example we can turn on the 3rd, 6th, and 7th LEDs.
  *          00100110 binary is 26 in hexadecimal, so we can program PORT 0 to be 0x26.              
